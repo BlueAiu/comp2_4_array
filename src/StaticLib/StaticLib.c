f9a3debf24480976f82636561248e03605a5f33c
BlueAiu <aiujustitia@yahoo.co.jp>
2023-11-13 21:38:54 +0900

    StaticLib.cを書き換えテストを通しました。

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -35,6 +35,22 @@ void release(my_array* ar)
 void resize(my_array* ar, int n)
 {
 	// ToOo:配列の要素数を変更しよう！(reallocは禁止)
+	int* newarray = (int*)malloc(sizeof(int) * n);
+	if (newarray == NULL) {
+		ar->num = 0;
+	}
+
+	for (int i = 0; i < n; i++) {
+		if (i < ar->num) {
+			newarray[i] = ar->addr[i];
+		}
+		else {
+			break;
+		}
+	}
+	free(ar->addr);
+	ar->addr = newarray;
+	ar->num = n;
 }
 
 // my_array のindex番目の要素にvalを設定する
@@ -42,7 +58,12 @@ void resize(my_array* ar, int n)
 bool set(my_array* ar, int index, int val)
 {
 	// ToOo:配列の要素を変更しよう！
-	return false;
+	if (index < 0 || index >= ar->num) {
+		return false;
+	}
+
+	ar->addr[index] = val;
+	return true;
 }
 
 // my_array のindex番目の要素を取得する
@@ -50,12 +71,17 @@ bool set(my_array* ar, int index, int val)
 int get(const my_array* ar, int index)
 {
 	// ToOo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+	if (index < 0 || index >= ar->num) {
+		return 0;
+	}
+
+	int val = ar->addr[index];
+	return val;
 }
 
 // my_array の要素数を取得する
 int size(const my_array* ar)
 {
 	// ToOo: 配列の要素数を返そう
-	return -1;
+	return ar->num;
 }
